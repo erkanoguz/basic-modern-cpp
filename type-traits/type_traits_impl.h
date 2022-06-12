8182feef5b3434ee9d5444a4a2ec8dcd63376e0b
erkanoguz <ooguzerkan@gmail.com>
2022-06-12 20:07:42 +0300

    integral_constant added

--- type-traits/type_traits_impl.h
+++ type-traits/type_traits_impl.h
@@ -3,18 +3,19 @@
 
 namespace exo {
 
-    struct false_type 
-    {
-        static constexpr bool value = false;
-        constexpr operator bool() const noexcept { return value; }
+    template <typename T, T v>
+    struct integral_constant {
+        static constexpr T value = v;
+        using value_type = T;
+        using type = integral_constant;
+        constexpr operator value_type() const noexcept { return value; }
+        constexpr value_type operator() () { return value; }
     };
 
-    struct true_type 
-    {
-        static constexpr bool value = true;
-        constexpr operator bool() const noexcept { return value; }
-    };
-
+    template <bool B>
+    using bool_constant = integral_constant<bool, B>;
+    using false_type = bool_constant<false>;
+    using true_type = bool_constant<true>;
 
     template<typename T> struct remove_reference { using type = T; };
     template<typename T> struct remove_reference<T&> { using type = T; };
@@ -49,6 +50,8 @@ namespace exo {
     template <typename T>
     struct is_pointer<T* const> : true_type {};
     
+    template<typename T>
+    inline constexpr bool is_pointer_v = is_pointer<T>::value;
 }
 
 
