eba39b2a3cedb7bd556c9d8a0a885c8f458bfed1
erkanoguz <ooguzerkan@gmail.com>
2022-06-17 10:19:47 +0300

    is_array unit test

--- test/type_traits_test.cpp
+++ test/type_traits_test.cpp
@@ -1,13 +1,24 @@
 #include "gtest/gtest.h"
 #include "../src/type_traits/type_traits_impl.h"
 
+#include <type_traits>
+#include <array>
+
 class TypeTraitsTest : public ::testing::Test
 {};
 
+class A {};
+
 TEST_F(TypeTraitsTest, TypeTraits_isArray)
 {
-    int a = 1;
-    int b = 1;
-
-    EXPECT_EQ(1, 1);
+    EXPECT_FALSE(exo::is_array<A>::value);
+    EXPECT_EQ(std::is_array<A>::value, exo::is_array<A>::value);
+    EXPECT_TRUE(exo::is_array<A[]>::value);
+    EXPECT_EQ(std::is_array<A[]>::value, exo::is_array<A[]>::value);
+    EXPECT_FALSE(exo::is_array<float>::value);
+    EXPECT_FALSE(exo::is_array<int>::value);
+    EXPECT_TRUE(exo::is_array<int[]>::value);
+    constexpr bool stdArray = std::is_array<std::array<int, 3>>::value;
+    constexpr bool exoArray = exo::is_array<std::array<int, 3>>::value;
+    EXPECT_EQ(stdArray, exoArray);
 }
